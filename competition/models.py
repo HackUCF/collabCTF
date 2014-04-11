c01ccb5d0f6cd4591dd5a2fe66c2ad53cd503407
Mark Ignacio <mark@ignacio,io>
2014-04-11 19:05:43 -0400

    base competition-level models

--- competition/models.py
+++ competition/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Competition(models.Model):
+    name = models.CharField('Name', max_length=255)
+    url = models.URLField('Competition URL', blank=True)
+    start_time = models.DateTimeField(blank=True)
+    end_time = models.DateTimeField(blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Challenge(models.Model):
+    name = models.CharField('Name', max_length=255)
+    progress = models.FloatField(default=0)
+    competition = models.ForeignKey(Competition, related_name='challenges')
+
+    def __unicode__(self):
+        return self.name

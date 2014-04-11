cf0fd8f060ed852621f6ef85f29f54ffdf3c6d5a
Mark Ignacio <mark@ignacio,io>
2014-04-11 18:47:08 -0400

    competition app skeleton

--- collabCTF/settings.py
+++ collabCTF/settings.py
@@ -23,7 +23,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south'
+    'south',
+    'competition'
 )
 
 MIDDLEWARE_CLASSES = (

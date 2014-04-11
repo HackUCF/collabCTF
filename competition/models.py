48d2b6e4dfdfb0a3d4db331c71f7d70fbe94530a
Mark Ignacio <mark@ignacio,io>
2014-04-11 19:41:51 -0400

    fleshed out challenge-level models

--- competition/models.py
+++ competition/models.py
@@ -1,3 +1,5 @@
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -12,9 +14,36 @@ class Competition(models.Model):
 
 
 class Challenge(models.Model):
+    PROGRESS_CHOICES = (
+        (0, 'Not Started'),
+        (1, 'In Progress'),
+        (2, 'Finished')
+    )
+
     name = models.CharField('Name', max_length=255)
-    progress = models.FloatField(default=0)
+    progress = models.PositiveSmallIntegerField(choices=PROGRESS_CHOICES)
+    num_progress = models.FloatField(default=0)
+    point_value = models.FloatField(default=0)
+
     competition = models.ForeignKey(Competition, related_name='challenges')
 
     def __unicode__(self):
         return self.name
+
+
+class ChallengeFile(models.Model):
+    file = models.FileField(upload_to='files/')
+    ctime = models.DateTimeField(auto_created=True)
+    mtime = models.DateTimeField(auto_now=True)
+    challenge = models.ForeignKey(Challenge, related_name='files')
+
+
+class Tag(models.Model):
+    tag = models.SlugField()
+    is_category = models.BooleanField(default=False)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+
+    def __unicode__(self):
+        return self.tag

e4562404d96a12f14c3d5434dc054d2525873669
Mark Ignacio <en.yaye@gmail.com>
2014-04-14 10:21:56 -0400

    removed node.js code from static files

--- competition/static/js/chat-client.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  var addr = "http://"+window.location.hostname+":7000";
-  console.log(addr);
-  var socket = io.connect(addr);
-  socket.on('new_message', function(data) {
-    console.log(data);
-  });
-});

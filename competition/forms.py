5c01d7a0bde326745b486b91ae42d920d748d5c0
broglea <austin.brogle@ucf.edu>
2014-04-14 19:21:42 -0400

    Aligned my buttons better on ctftools.html

--- competition/forms.py
+++ competition/forms.py
@@ -1,6 +1,7 @@
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Reset, Layout, Fieldset, ButtonHolder
 from django import forms
+from django.core.urlresolvers import reverse
 
 from competition.models import Challenge, Competition
 
@@ -11,7 +12,6 @@ _form_control = {'class': 'form-control'}
 class CompetitionModelForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(CompetitionModelForm, self).__init__(*args, **kwargs)
-        fieldset_title = kwargs.get('fieldset_title', 'Add a competition')
         self.helper = FormHelper()
         self.helper.form_id = 'add-ctf'
         self.helper.form_method = 'post'
@@ -19,7 +19,7 @@ class CompetitionModelForm(forms.ModelForm):
 
         self.helper.layout = Layout(
             Fieldset(
-                fieldset_title,
+                'Add a competition',
                 'name',
                 'url',
                 'start_time',
@@ -95,3 +95,22 @@ class BaseConversionForm(forms.Form):
 class XorForm(forms.Form):
     value = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'cols': 40}))
     key = forms.CharField(widget=forms.TextInput(attrs={'size': 40}))
+
+    def __init__(self, *args, **kwargs):
+        super(XorForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'xor-form'
+        self.helper.form_method = 'post'
+        self.helper.form_action = reverse("tools_xor")
+        self.helper.layout = Layout(
+            Fieldset(
+                'XOR Strings',
+                'value',
+                'key'
+            ),
+            ButtonHolder(
+                Submit('submit', 'Submit'),
+                Reset('reset', 'Reset'),
+                css_class='text-right'
+            )
+        )
